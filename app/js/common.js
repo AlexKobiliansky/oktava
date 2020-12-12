022ed020319f9670567625c1c027f40c1ef89234
alex <alex_kobilianskiy@bigmir.net>
2020-12-12 19:20:23 +0200

    header complete (without adaptation)

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,38 @@
 $(document).ready(function(){
+    var element = document.querySelector( '.main-mnu' );
+
+    var droppy = new Droppy( element, {
+        parentSelector: 'li',
+        dropdownSelector: 'li > ul',
+        triggerSelector: 'a'
+    } );
+
+    $('.main-mnu li ul li').on('mouseenter', function(){
+        $(this).addClass('hovered').siblings('li').removeClass('hovered');
+    });
+
+    // $('.droppy__parent').click(function(){
+    //     $(this).toggleClass('opened');
+    // })
+
+
+
+
+    $('.droppy__parent').on("mouseenter", function(){
+        $(this).addClass('opened');
+        $(this).children('.droppy__drop ').addClass('droppy__drop--active')
+    });
+
+    $('.droppy__parent').on("mouseleave", function(){
+
+        let th = $(this);
+
+        // setTimeout(function(){
+            th.removeClass('opened');
+            th.children('.droppy__drop ').removeClass('droppy__drop--active')
+        // }, 2000);
+    });
+
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
