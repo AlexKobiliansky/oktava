c5eb79e68a4396c47bfd120b30cbd9eecbdee605
alex <alex_kobilianskiy@bigmir.net>
2020-12-13 00:49:50 +0200

    header complete (adaptation)

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,47 @@
 $(document).ready(function(){
+
+    /** mobile-mnu customization */
+    var mmenu = $('#mobile-mnu');
+    var menuLogo = mmenu.data("logo");
+    var $mmenu = mmenu.mmenu({
+        navbars: [{
+            content: [ "<img src=" + menuLogo + " class=\"img-responsive mm-logo\" alt=\"alt\"/>" ],
+            height: 3
+        }],
+        "pageScroll": true,
+
+        "navbar": {
+            "title" : "",
+        },
+        "extensions": [
+            "theme-dark",
+            "pagedim-black",
+            "position-front",
+            "fx-listitems-slide",
+        ],
+    }, {
+        offCanvas: {
+            pageSelector: "#page-container"
+        },
+    });
+
+    var mmenuBtn = $("#mmenu-btn");
+    var API = $mmenu.data("mmenu");
+
+    mmenuBtn.click(function() {
+        API.open();
+        $(this).addClass('is-active')
+    });
+
+
+    API.bind( "close:start", function() {
+        setTimeout(function() {
+            mmenuBtn.removeClass( "is-active" );
+        }, 300);
+    });
+    /** end mobile-mnu customization */
+
+
     var element = document.querySelector( '.main-mnu' );
 
     var droppy = new Droppy( element, {
@@ -11,12 +54,6 @@ $(document).ready(function(){
         $(this).addClass('hovered').siblings('li').removeClass('hovered');
     });
 
-    // $('.droppy__parent').click(function(){
-    //     $(this).toggleClass('opened');
-    // })
-
-
-
 
     $('.droppy__parent').on("mouseenter", function(){
         $(this).addClass('opened');
@@ -24,15 +61,21 @@ $(document).ready(function(){
     });
 
     $('.droppy__parent').on("mouseleave", function(){
-
-        let th = $(this);
-
-        // setTimeout(function(){
-            th.removeClass('opened');
-            th.children('.droppy__drop ').removeClass('droppy__drop--active')
-        // }, 2000);
+        $(this).removeClass('opened');
+        $(this).children('.droppy__drop ').removeClass('droppy__drop--active')
     });
 
+
+
+
+
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 30) {
+            $('.main-head').addClass('sticky')
+        } else {
+            $('.main-head').removeClass('sticky')
+        }
+    });
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
