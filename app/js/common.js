61275e3ffc2c02ca3062b0d927ea3f52f2dec008
alex <alex_kobilianskiy@bigmir.net>
2020-12-13 18:59:59 +0200

    section intro complete

--- app/js/common.js
+++ app/js/common.js
@@ -67,14 +67,156 @@ $(document).ready(function(){
 
 
 
+    // new CircleType(document.getElementById('intro-curve'));
+        new CircleType(document.getElementById('intro-curve'));
+
+        $('.curve-span').css('animation-name', 'rotateAnimation')
+
+
+
+
+    let owl = $('.intro-slider');
+    let $introCurrent = $('#intro-current');
+    let $introTotal = $('#intro-total');
+    let $slides = owl.find('.intro-slide').length;
+
+    owl.on('initialized.owl.carousel', function(e){
+        $introTotal.text($slides);
+        let currentSlideIndex = e.item.index + 1;
+
+
+
+        let currentSlide = $(".intro-slider .owl-item:nth-child("+currentSlideIndex+")");
+        let prevSlide = $(".intro-slider .owl-item:nth-child("+(currentSlideIndex-1)+")");
+        let nextSlide = $(".intro-slider .owl-item:nth-child("+(currentSlideIndex+1)+")");
+
+
+        let curItem = currentSlide.find('.intro-slide');
+        let prevItem = prevSlide.find('.intro-slide');
+        let nextItem = nextSlide.find('.intro-slide');
+
+
+
+        let curItemPreview = curItem.data('previewname');
+        let prevItemPreview = prevItem.data('previewname');
+        let nextItemPreview = nextItem.data('previewname');
+
+        let curItemNumber = curItem.data('number');
+
+        $('.intro-nav-current span').text(curItemPreview)
+        $('.intro-nav-prev span').text(prevItemPreview)
+        $('.intro-nav-next span').text(nextItemPreview)
+
+
+    })
+
+
+    owl.owlCarousel({
+        loop:true,
+        nav:false,
+        items: 1,
+        dots: false,
+        animateIn: "fadeIn",
+        animateOut: "fadeOut",
+        // mouseDrag: false,
+        touchDrag: false,
+        smartSpeed:700,
+        // autoplay: true,
+        autoplayTimeout: 7000,
+        autoplaySpeed: 2000,
+        // navText: false,
+        // responsive: {
+        //     0: {
+        //         nav: false
+        //     },
+        //     1300: {
+        //         nav: true
+        //     }
+        // }
+    });
+
+    owl.on('changed.owl.carousel', function(e) {
+
+
+        let currentSlideIndex = e.item.index + 1;
+
+
+
+        let currentSlide = $(".intro-slider .owl-item:nth-child("+currentSlideIndex+")");
+        let prevSlide = $(".intro-slider .owl-item:nth-child("+(currentSlideIndex-1)+")");
+        let nextSlide = $(".intro-slider .owl-item:nth-child("+(currentSlideIndex+1)+")");
+
+
+        let curItem = currentSlide.find('.intro-slide');
+        let prevItem = prevSlide.find('.intro-slide');
+        let nextItem = nextSlide.find('.intro-slide');
+
+
+
+        let curItemPreview = curItem.data('previewname');
+        let prevItemPreview = prevItem.data('previewname');
+        let nextItemPreview = nextItem.data('previewname');
+
+        let curItemNumber = curItem.data('number');
+        $introCurrent.text(curItemNumber);
+
+        $('.intro-nav-current span').text(curItemPreview)
+        $('.intro-nav-prev span').text(prevItemPreview)
+        $('.intro-nav-next span').text(nextItemPreview)
+
+
+    });
+
+
+    $('.intro-nav-prev').click(function() {
+        owl.trigger('prev.owl.carousel', 2000);
+    });
+
+    $('.intro-nav-next').click(function() {
+        owl.trigger('next.owl.carousel', 2000);
+    });
+
+
 
 
     $(window).scroll(function() {
-        if($(this).scrollTop() > 30) {
+        if($(this).scrollTop() > 60) {
             $('.main-head').addClass('sticky')
         } else {
             $('.main-head').removeClass('sticky')
         }
+    });
+
+    $('img.svg').each(function(){
+        var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+            if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+            }
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
     });
 
     //E-mail Ajax Send
